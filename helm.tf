eebc77733654d099722496d4b3c8e17d0376acd1
Migara Ekanayake <mekanayake@paloaltonetworks.com>
2020-04-13 21:42:47 +0100

    helm tf

---
+++ helm.tf
@@ -0,0 +1,25 @@
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.primary.endpoint}"
+    token                  = "${data.google_client_config.default.access_token}"
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+    load_config_file       = false
+  }
+}
+
+data "google_client_config" "default" {}
+
+data "helm_repository" "incubator" {
+  name = "incubator"
+  url  = "https://migara.github.io/sock-shop-demo"
+}
+
+output "name" {
+  value = data.helm_repository.incubator.metadata[0].name
+}
+
+# resource "helm_release" "my_cache" {
+#   name       = "my-cache"
+#   repository = data.helm_repository.incubator.metadata[0].name
+#   chart      = "redis-cache"
+# }

b3620792f2bebe144d4846e14fa876129181028d
Migara Ekanayake <mekanayake@paloaltonetworks.com>
2020-04-14 01:13:14 +0100

    helm tf

--- main.tf
+++ main.tf
@@ -2,12 +2,7 @@ provider "google" {
   region = var.region
 }
 
-provider "google-beta" {
-  region = var.region
-}
-
 resource "google_container_cluster" "primary" {
-  provider = google-beta
   name     = var.cluster_name
   location = var.location
 
@@ -15,7 +10,7 @@ resource "google_container_cluster" "primary" {
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
-  initial_node_count       = 3
+  initial_node_count       = 1
 
   master_auth {
     username = ""
@@ -26,16 +21,13 @@ resource "google_container_cluster" "primary" {
     }
   }
 
-  cluster_autoscaling {
-    enabled = true
-  }
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "me-node-pool"
   location   = var.location
   cluster    = google_container_cluster.primary.name
-  node_count = 1
+  node_count = 3
 
   node_config {
     preemptible  = true
@@ -49,6 +41,11 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
+  }
+
+  autoscaling {
+    min_node_count = 3
+    max_node_count = 8
   }
 }
 
